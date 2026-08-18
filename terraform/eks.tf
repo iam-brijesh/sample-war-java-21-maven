9f27988e41c4f56dec7a8ee9020f7ed4a8cafeb0
Ubuntu <ubuntu@ip-172-31-10-130.ap-south-1.compute.internal>
2026-08-18 18:23:30 +0000

    Add EKS infrastructure configuration

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -12,7 +12,7 @@ module "eks" {
 
   enable_cluster_creator_admin_permissions = true
 
-  cluster_addons = {
+  addons = {
     coredns = {
       most_recent = true
     }
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,11 +1,35 @@
 output "cluster_name" {
-  value = aws_eks_cluster.eks.name
+  description = "EKS cluster name"
+  value       = module.eks.cluster_name
 }
 
 output "cluster_endpoint" {
-  value = aws_eks_cluster.eks.endpoint
+  description = "EKS cluster API endpoint"
+  value       = module.eks.cluster_endpoint
+}
+
+output "cluster_arn" {
+  description = "EKS cluster ARN"
+  value       = module.eks.cluster_arn
+}
+
+output "vpc_id" {
+  description = "VPC ID"
+  value       = module.vpc.vpc_id
+}
+
+output "private_subnet_ids" {
+  description = "Private subnet IDs"
+  value       = module.vpc.private_subnets
+}
+
+output "public_subnet_ids" {
+  description = "Public subnet IDs"
+  value       = module.vpc.public_subnets
 }
 
 output "ecr_repository_url" {
-  value = aws_ecr_repository.sample_war.repository_url
+  description = "ECR repository URL"
+  value       = aws_ecr_repository.sample_war.repository_url
 }
+
