a9a9484e5fe3b0e9c39c9debc4f2648ec0f92c10
iam-brijesh <iambrijesh.b@gmail.com>
2026-08-19 17:10:42 +0530

    Add kubernetes_version to EKS node group
    Added kubernetes_version to the default node group configuration.

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -50,6 +50,9 @@ module "eks" {
     default = {
       name = "${var.cluster_name}-nodegroup"
 
+      # IMPORTANT
+      kubernetes_version = var.kubernetes_version
+
       iam_role_use_name_prefix = false
       iam_role_name            = "${var.cluster_name}-node-role"
 
