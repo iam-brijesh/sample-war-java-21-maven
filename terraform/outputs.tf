0e472a359b6e9b03fdace63c56380e14b955a40a
iam-brijesh <iambrijesh.b@gmail.com>
2026-08-20 00:30:29 +0530

    Refactor outputs in outputs.tf with descriptions

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,32 +1,39 @@
-output "vpc_id" {
-  value = data.aws_vpc.existing.id
-}
-
-output "public_subnet_ids" {
-  value = data.aws_subnets.public.ids
-}
-
-output "private_subnet_ids" {
-  value = data.aws_subnets.private.ids
-}
-
 output "cluster_name" {
-  value = module.eks.cluster_name
-}
-
-output "cluster_arn" {
-  value = module.eks.cluster_arn
+  description = "EKS cluster name"
+  value       = module.eks.cluster_name
 }
 
 output "cluster_endpoint" {
-  value = module.eks.cluster_endpoint
+  description = "EKS cluster endpoint"
+  value       = module.eks.cluster_endpoint
 }
 
-output "node_group_id" {
-  description = "EKS managed node group ID"
-  value       = module.eks.eks_managed_node_groups["default"].node_group_id
+output "cluster_version" {
+  description = "EKS Kubernetes version"
+  value       = var.kubernetes_version
+}
+
+output "vpc_id" {
+  description = "Existing VPC ID"
+  value       = data.aws_vpc.existing.id
+}
+
+output "public_subnet_ids" {
+  description = "Existing public subnet IDs"
+  value       = data.aws_subnets.public.ids
+}
+
+output "private_subnet_ids" {
+  description = "Existing private subnet IDs"
+  value       = data.aws_subnets.private.ids
+}
+
+output "node_group_name" {
+  description = "EKS managed node group name"
+  value       = module.eks.eks_managed_node_groups["default"].node_group_name
 }
 
 output "ecr_repository_url" {
-  value = aws_ecr_repository.sample_war.repository_url
+  description = "ECR repository URL"
+  value       = aws_ecr_repository.sample_war.repository_url
 }
