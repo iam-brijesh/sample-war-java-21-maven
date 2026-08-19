519a42f828d19be6e48fd10fd8c3a6c41bb4ba54
iam-brijesh <iambrijesh.b@gmail.com>
2026-08-19 15:19:14 +0530

    Clean up comments in eks.tf
    Removed comment about IAM role name prefix limit.

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -50,7 +50,6 @@ module "eks" {
     default = {
       name = "${var.cluster_name}-nodegroup"
 
-      # Fix: Prevent auto-generated IAM role name prefix from exceeding the 38-character limit
       iam_role_use_name_prefix = false
       iam_role_name            = "${var.cluster_name}-node-role"
 
